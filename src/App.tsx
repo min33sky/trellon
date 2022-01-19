aaa62312c2360d6d29e71e47079b0e662046552b
min33sky <min33sky@naver.com>
2022-01-19 17:32:54 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,143 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { darkTheme } from './theme';
+
+const GlobalStyle = createGlobalStyle`
+  /* http://meyerweb.com/eric/tools/css/reset/
+    v2.0 | 20110126
+    License: none (public domain)
+  */
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.bgColor};
+    color: black;
+    font-weight: 300;
+    line-height: 1.2;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 480px;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr); //? 3, 1fr로 바꿀꺼임
+`;
+
+const Board = styled.ul`
+  background-color: ${(props) => props.theme.boardColor};
+  padding: 20px 10px;
+  padding-top: 30px;
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.li`
+  background-color: ${(props) => props.theme.cardColor};
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+`;
+
+const toDos = ['a', 'b', 'c', 'd', 'e', 'f'];
 
 function App() {
+  const onDragEnd = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+
+        {/* 드래그 앤 드랍할 수 있는 컴포넌트  */}
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Wrapper>
+            <Boards>
+              <Droppable droppableId="one">
+                {(magic) => (
+                  <Board ref={magic.innerRef} {...magic.droppableProps}>
+                    {toDos.map((toDo, index) => (
+                      <Draggable draggableId={toDo} index={index}>
+                        {(magic) => (
+                          <Card
+                            ref={magic.innerRef}
+                            {...magic.dragHandleProps}
+                            {...magic.draggableProps}
+                          >
+                            {toDo}
+                          </Card>
+                        )}
+                      </Draggable>
+                    ))}
+                    {/* 드래그 하는 동안에 드롭 공간 크기 변동을 막아준다. */}
+                    {magic.placeholder}
+                  </Board>
+                )}
+              </Droppable>
+            </Boards>
+          </Wrapper>
+        </DragDropContext>
+      </ThemeProvider>
+    </>
   );
 }
 
